2ca12fdc30b6d5965a2d91e096b11edb05a6fa9d
sarkis7412 <sarkis_aghazarian@yahoo.com>
2018-11-21 21:34:44 -0800

    working on dynamic dropdown

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 'use strict';
 
-// //getting data from file
-// $.get("data/page-1.json").then(data => {});
-
-console.log('test');
-
 //render pics to page
 const hornsGallery = [];
+const keywords = [];
+const keywordsFinal = [0];
 
 function horns(obj) {
     this.title = obj.title;
     this.image_url = obj.image_url;
     this.description = obj.description;
+    this.keyowrd = obj.keyword;
 
     hornsGallery.push(this);
+    keywords.push(obj.keyword)
 }
 
     horns.prototype.render = function() {
@@ -22,7 +21,7 @@ function horns(obj) {
     let $clone = $('div[class="clone"]');
     let hornsTemplate = $('#photo-template').html();
     $clone.html(hornsTemplate);
-    $clone.find('h1').text(this.title);
+    $clone.find('h2').text(this.title);
     $clone.find('p').text(this.description);
     $clone.find('img').attr('src', this.image_url);
     $clone.removeClass('clone');
@@ -45,12 +44,43 @@ function horns(obj) {
       hornsGallery.forEach(horns =>{
       horns.render();
   
+      hornsGallery.forEach(horns => {
+        console.log()
+      })
         })
   
       })
   }
    
   $(() => readJson());
+
+  const popFilter = function() {
+    keywords.forEach(element => {
+      console.log(element);
+      var doIt = true;
+      for(let i in keywordsFinal) {
+        if(element === keywordsFinal[i]) {
+          doIt = false;
+          break;
+        }
+
+      }
+      if(doIt === true) {
+        keywordsFinal.push(element);
+      }
+      }) 
+      
+  }
+
+  popFilter();
+
+//   $.get('data/page-1.json', 'json')
+//   .then(data => {}
+//   var $dropdown = $("#dropdown");
+//   $.each(result, function() {
+//   $dropdown.append($("<option />").text(this.title));
+// });
+  
 
 
 //add button to hide all pics but ones with selected data types
