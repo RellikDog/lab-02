99bb1c0158d2e6adb6250b5ee456f694c4e73435
Richard Bellamy <richard.bellamy123@gmail.com>
2018-11-21 21:34:12 -0800

    css

--- js/main.js
+++ js/main.js
@@ -8,49 +8,49 @@ console.log('test');
 //render pics to page
 const hornsGallery = [];
 
-function horns(obj) {
-    this.title = obj.title;
-    this.image_url = obj.image_url;
-    this.description = obj.description;
+function Horns(obj) {
+  this.title = obj.title;
+  this.image_url = obj.image_url;
+  this.description = obj.description;
 
-    hornsGallery.push(this);
+  hornsGallery.push(this);
 }
 
-    horns.prototype.render = function() {
+Horns.prototype.render = function() {
 
-    $('main').append('<div class="clone"></div>');
-    let $clone = $('div[class="clone"]');
-    let hornsTemplate = $('#photo-template').html();
-    $clone.html(hornsTemplate);
-    $clone.find('h1').text(this.title);
-    $clone.find('p').text(this.description);
-    $clone.find('img').attr('src', this.image_url);
-    $clone.removeClass('clone');
-    $clone.attr('class', this.title);  
-  
-  }
+  $('main').append('<div class="clone"></div>');
+  let $clone = $('div[class="clone"]');
+  let hornsTemplate = $('#photo-template').html();
+  $clone.html(hornsTemplate);
+  $clone.find('h2').text(this.title);
+  $clone.find('p').text(this.description);
+  $clone.find('img').attr('src', this.image_url);
+  $clone.removeClass('clone');
+  $clone.attr('class', this.title);
+
+}
 
 
-  function readJson () {
+function readJson () {
 
-    $.get('data/page-1.json', 'json')
-  
-      .then(data => {
+  $.get('data/page-1.json', 'json')
+
+    .then(data => {
       data.forEach(hornsObj => {
-        new horns(hornsObj);
-        })
-    })
-  
-      .then(function() {
-      hornsGallery.forEach(horns =>{
-      horns.render();
-  
-        })
-  
+        new Horns(hornsObj);
       })
-  }
-   
-  $(() => readJson());
+    })
+
+    .then(function() {
+      hornsGallery.forEach(horns =>{
+        horns.render();
+
+      })
+
+    })
+}
+
+$(() => readJson());
 
 
 //add button to hide all pics but ones with selected data types
